f90b2ce5b559b093b5d00b8118cb0322b425c540
zendar403 <83726376+zendar403@users.noreply.github.com>
2021-05-20 15:28:26 +0300

    Update onayla.js

--- komutlar/onayla.js
+++ komutlar/onayla.js
@@ -3,23 +3,23 @@ const db = require('quick.db');
 const botlist  = require('../botlist.json')
 
 exports.run = async(client, message, args) => {
-	if(!message.member.roles.cache.has(botlist.yetkili)) return message.channel.send("Yeterli yetkin Yok.")
-  if(message.channel.id !== botlist.kanalyetkili) return message.channel.send(`<#${botlist.kanalyetkili}> sadece bu kanalda kullanabılır.`)
+	if(!message.member.roles.cache.has(botlist.yetkili)) return message.channel.send("You can't use this command.")
+  if(message.channel.id !== botlist.kanalyetkili) return message.channel.send(`<#${botlist.kanalyetkili}>`)
 
 let botid = args[0]
 let sahip = args[1]
-if(!botid) return message.channel.send("Bir Bot İd Gir.").then(a => a.delete(20000))
-if(!sahip) return message.channel.send("Sahip İd Giriniz.").then(a => a.delete(20000))
+if(!botid) return message.channel.send("Enter a bot ID.").then(a => a.delete(20000))
+if(!sahip) return message.channel.send("Enter owner's ID.").then(a => a.delete(20000))
 message.delete()
   
-message.channel.send("✅ **|** Başarıyla Botu Onayladınız.")
+message.channel.send("✅ **|** Bot Approved")
   
    message.guild.members.cache.get(botid).roles.add(botlist.botrol)
  message.guild.members.cache.get(sahip).roles.add(botlist.onaylıüye)
   
   let embed = new Discord.MessageEmbed()
   .setColor("RANDOM")
-.setDescription(`:white_check_mark:  **|**  <@${botid}> Adlı Bot Onaylandı Bot Sahip : <@${sahip}>.`)
+.setDescription(`:white_check_mark:  **|**  <@${botid}> Got Approved. Bot Owner: <@${sahip}>.`)
 client.channels.cache.get(botlist.log).send(embed)
   
 
@@ -32,7 +32,7 @@ exports.conf = {
 };
 
 exports.help = {
-	name: "onayla"
+	name: "approve"
 }
 
 
