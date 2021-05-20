bbc142513ea398e3c1b8147ea79618a8b99fcc81
zendar403 <83726376+zendar403@users.noreply.github.com>
2021-05-20 15:26:30 +0300

    Update botekle.js

--- komutlar/botekle.js
+++ komutlar/botekle.js
@@ -4,25 +4,25 @@ const botlist  = require('../botlist.json')
 
 exports.run = async(client, message, args) => {
 
-  if(message.channel.id !== botlist.başvurukanal) return message.channel.send(`<#${botlist.başvurukanal}> sadece bu kanalda kullanabılır.`)
+  if(message.channel.id !== botlist.başvurukanal) return message.channel.send(`<#${botlist.başvurukanal}>`)
 
 
 let botid = args[0]
 let prefix = args[1]
-if(!botid) return message.channel.send("Bir Bot İd Gir.")
-if(!prefix) return message.channel.send("Prefix Giriniz.")
+if(!botid) return message.channel.send("Enter a valid bot ID.")
+if(!prefix) return message.channel.send("Enter your bot's prefix.")
 message.delete()
   
-message.channel.send("✅ **|** Botunuz Sisteme Eklendi Lütfen Onay Almasını Bekleyınız.")
+message.channel.send("✅ **|** Please wait until your bot gets approved, then you can test it.")
   
   client.channels.cache.get(botlist.log).send(`<@&${botlist.yetkili}>`)
   
   let embed = new Discord.MessageEmbed()
   .setColor("RANDOM")
-  .addField(`Bot İd`, ` \`\`\`${botid}\`\`\` `)
-   .addField(`Bot Sahip`, ` \`\`\`${message.author.tag}\`\`\` `)
+  .addField(`Bot ID`, ` \`\`\`${botid}\`\`\` `)
+   .addField(`Bot Owner`, ` \`\`\`${message.author.tag}\`\`\` `)
    .addField(`Prefix`, ` \`\`\`${prefix}\`\`\` `)
-  .addField(`Ekle`,`[0 Perm](https://discord.com/api/oauth2/authorize?client_id=${botid}&permissions=0&scope=bot) **|** [8 Perm](https://discord.com/api/oauth2/authorize?client_id=${botid}&permissions=8&scope=bot)`)
+  .addField(`Add`,`[0 Perm](https://discord.com/api/oauth2/authorize?client_id=${botid}&permissions=0&scope=bot) **|** [8 Perm](https://discord.com/api/oauth2/authorize?client_id=${botid}&permissions=8&scope=bot)`)
 client.channels.cache.get(botlist.log).send(embed)
   
 
@@ -35,7 +35,7 @@ exports.conf = {
 };
 
 exports.help = {
-	name: "ekle"
+	name: "add"
 }
 
 
